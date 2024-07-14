16a6b94da39d6902dd0153bfcfd5e0ebc74d087c
bzzoo <fullbster@gmail.com>
2024-07-14 16:36:04 +0900

    [FEAT] 레이아웃: 메인 레이아웃 뼈대 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Noto_Sans_KR } from "next/font/google";
+import "@/app/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const notoSansKR = Noto_Sans_KR({ weight: ["500"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${notoSansKR.className} bg-neutral-400/10 antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
